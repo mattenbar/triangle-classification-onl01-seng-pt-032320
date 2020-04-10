86c4a74b0afb0be90999f49912801d86c0301031
matthew enbar <matthew.enbar@gmail.com>
2020-04-10 18:14:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_accessor :base, :height, :hypotenuse, :equilateral, :isosceles, :scalene
+
+  def initialize (base, height, hypotenuse)
+    @base = base
+    @height = height
+    @hypotenuse = hypotenuse
+  end
+
+  def kind
+    if @base <= 0 || @height <= 0 || @hypotenuse <= 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif @base == @height && @height == @hypotenuse
+      return :equilateral
+
+    elsif (@base == @height && @height != @hypotenuse) || (@base == @hypotenuse && @hypotenuse != @height) || (@height == @hypotenuse && @height != @base)
+      return :isosceles
+
+    else @base != @height && @height != @hypotenuse && @base != @hypotenuse
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "this is not a triangle"
+    end
+  end
 end
